c6b2fae7936886a86dcaf54ce7cdadd2ed5ffb13
prathmeshsikchi <66197972+prathmeshsikchi@users.noreply.github.com>
2020-06-20 22:11:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function()
 {
-	var name = ["Jhon" , "jack" , "jim" , "josef" , "yakov" , "Paul" , "laura" , "Jason"];
+	var name = ["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
 	for (var i = 0 ; i < name.length;i++)
 	{	
 		var fir_letter = name[i].charAt(0).toLowerCase();
